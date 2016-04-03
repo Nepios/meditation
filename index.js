6377a7bd7d603f2b771d56038e0137c9a90b670a
Nepios <nepios2@gmail.com>
2016-04-03 13:37:20 -0700

    basic app setup

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var app = express();
+
+var secret = "mysupersecretpassword";
+
+var mongoose = require('mongoose');
+var User = require('./models/user');
+mongoose.connect('mongodb://localhost/meditations');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
